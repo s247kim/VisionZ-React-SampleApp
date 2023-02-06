6b05f7ead6850fa5cc7066e60836f1b74a5aec93
Simon <s247kim@gmail.com>
2023-02-06 00:01:03 -0800

    added checkbox to tabindex

--- src/components/shared/checkbox/checkbox.component.tsx
+++ src/components/shared/checkbox/checkbox.component.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, KeyboardEvent } from "react";
 
 import styles from "./checkbox.styles.module.scss";
 
@@ -15,7 +15,27 @@ export const Checkbox: FC<CheckboxProps> = ({ checked: isChecked, disabled: isDi
         }
     };
 
-    return <span className={[styles.checkbox, isDisabled ? styles.disabled : ""].join(" ")} onClick={changeCheckedState}>
+    const handleKeyDown = (event: KeyboardEvent<HTMLSpanElement>) => {
+        if (isDisabled && !handleCheckedStateChange) return;
+
+        const { code, altKey, ctrlKey, shiftKey } = event;
+
+        switch (code.toLowerCase()) {
+            case "space": {
+                if (!altKey && !ctrlKey && !shiftKey) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    handleCheckedStateChange!(!isChecked);
+                }
+                return;
+            }
+        }
+    };
+
+    return <span tabIndex={isDisabled ? -1 : 0}
+                 className={[styles.checkbox, isDisabled ? styles.disabled : ""].join(" ")}
+                 onClick={changeCheckedState}
+                 onKeyDown={handleKeyDown}>
         {isChecked && <div className={"checker"}></div>}
     </span>;
 };
